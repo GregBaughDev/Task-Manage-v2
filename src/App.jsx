257e54b851e7ff4f8d6db099445ec285d6b8307d
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-09-09 14:34:31 +1000

    WIP- Login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import { GlobalStyle } from './styles';
 import TaskPage from './components/TaskPage';
+import Login from './components/Login';
 
 function App() {
+  const [userAuth, setUserAuth] = useState(false)
+  
+//TO DO - React router dom and show the component if user is logged in/out
+
   return (
     <>
       <GlobalStyle />
-      <TaskPage />
+      {!userAuth ? 
+        <Login userAuth={userAuth} setUserAuth={setUserAuth} /> :
+        <TaskPage />
+      }
     </>
   );
 }
